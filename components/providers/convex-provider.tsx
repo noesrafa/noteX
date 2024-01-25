dfeb6a37976ce4f36b6c56eb29ca627f27140651
Rafael Alexander <rafael.tena@heru-app.com>
2024-01-24 21:39:17 -0600

    envs

--- components/providers/convex-provider.tsx
+++ components/providers/convex-provider.tsx
@@ -5,12 +5,12 @@ import { ConvexReactClient } from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ClerkProvider, useAuth } from "@clerk/clerk-react";
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL ?? "");
 
 const ConvexProviderClient = ({ children }: { children: ReactNode }) => {
   return (
     <ClerkProvider
-      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY ?? ""}
     >
       <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
         {children}
